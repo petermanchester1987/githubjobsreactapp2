16b63dcc3b65406030d319cca6a47bcc4c979442
peter <64981120+petermanchester1987@users.noreply.github.com>
2020-09-18 21:01:31 +0100

    attempted deploy 2

--- server.js
+++ server.js
@@ -6,13 +6,15 @@ const app = express();
 
 app.use(bodyParser.json());
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
 const port = process.env.PORT || 3000;
-
-app.use(express.static("client/build"));
-
-app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-});
 
 app.listen(port, () => {
   console.log(`Server is up on port ${port}!`);
